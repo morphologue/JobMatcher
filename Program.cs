e5b49e2e26a4b3b55ff169986e319fca2e124cdf
Gavin Heise <morphologue@users.noreply.github.com>
2018-04-24 14:49:55 +1000

    Apply and de-cruft .NET Core 2.0 MVC template

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace JobMatcher
+{
+    public class Program
+    {
+        public static void Main(string[] args) => BuildWebHost(args).Run();
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+        }
+    }
+}
